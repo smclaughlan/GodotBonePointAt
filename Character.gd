c3b073f026e4501ca76f695766cc4fac50ec86dd
smclaughlan <sean.s.mclaughlan@gmail.com>
2023-10-17 23:28:33 -0700

    Update Character2.gd to use alternate look at method

--- Character.gd
+++ Character.gd
@@ -10,8 +10,6 @@ func _process(delta):
 	var player_vec = transform.basis.z
 	var dir_to_targ = global_position.direction_to(look_at_target.global_position)
 	var dotprod = player_vec.dot(dir_to_targ)
-#	<0 or >0 will be in front/behind.
-#    print('dotprod: ', dotprod) # should be -1 to 1
 	if dotprod < 0:
 		var quat = Quaternion(Vector3(0, 0, 1), 0)
 		# Smoothly transition with a tween.
@@ -24,7 +22,6 @@ func _process(delta):
 	var angle1 = player_skeleton.get_bone_global_pose(upper_body).origin.direction_to(to_local(look_at_target.global_position))
 	# Be careful about which axis is set to 1.
 	var quat = Quaternion(Vector3(0, 0, 1), angle1)
-	
 	# Smoothly transition with a tween.
 	var tween = get_tree().create_tween()
 	tween.tween_property(player_skeleton, "bones/" + str(upper_body) + "/rotation", quat, 1)
--- Character2.gd
+++ Character2.gd
@@ -10,23 +10,20 @@ func _process(delta):
 	var player_vec = transform.basis.z
 	var dir_to_targ = global_position.direction_to(look_at_target.global_position)
 	var dotprod = player_vec.dot(dir_to_targ)
-#	<0 or >0 will be in front/behind.
-#    print('dotprod: ', dotprod) # should be -1 to 1
 	if dotprod < 0:
+		# Tween back to base rotation.
 		var quat = Quaternion(Vector3(0, 0, 1), 0)
-		# Smoothly transition with a tween.
+#		# Smoothly transition with a tween.
 		var tween = get_tree().create_tween()
 		tween.tween_property(player_skeleton, "bones/" + str(upper_body) + "/rotation", quat, 1)
 		# Or instantly snap.
 #		player_skeleton.set_bone_pose_rotation(upper_body, quat)
 		return
-	# Otherwise do the look at behavior
-	var angle1 = player_skeleton.get_bone_global_pose(upper_body).origin.direction_to(to_local(look_at_target.global_position))
-	# Be careful about which axis is set to 1.
-	var quat = Quaternion(Vector3(0, 0, 1), angle1)
-	
-	# Smoothly transition with a tween.
+	# Rotate and tween to look at target.
+	var lookRotation = player_skeleton.get_bone_global_pose_no_override(upper_body)
+	lookRotation = lookRotation.looking_at(to_local(look_at_target.global_position), Vector3(0, 1, 0), true)
+	# Snap
+#	player_skeleton.set_bone_global_pose_override(upper_body, lookRotation, 1.0, true)
+	# Or tween.
 	var tween = get_tree().create_tween()
-	tween.tween_property(player_skeleton, "bones/" + str(upper_body) + "/rotation", quat, 1)
-	# Or instantly snap.
-#	player_skeleton.set_bone_pose_rotation(upper_body, quat)
+	tween.tween_property(player_skeleton, "bones/" + str(upper_body) + "/rotation", Quaternion(lookRotation.basis), 1)
